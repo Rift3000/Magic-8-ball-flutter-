d578d413861bc0010a52bb570370c3302d35da14
Jason Williams <williamsjacen007@gmail.com>
2020-04-02 17:58:48 -0500

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[200],
+      appBar: AppBar(
+        title: Center(child: Text("Ask Me Anything")),
+        backgroundColor: Colors.blue[800],
+      ),
+      body: Magic(),
+    );
+  }
+}
+
+class Magic extends StatefulWidget {
+  @override
+  _MagicState createState() => _MagicState();
+}
+
+class _MagicState extends State<Magic> {
+  int roller = 0;
+  void roadRoller() {
+    setState(() {
+      roller = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlatButton(
+            onPressed: () {
+              roadRoller();
+            },
+            child: Image.asset("images/ball$roller.png"),
+          ),
+        ],
+      ),
+    );
+  }
+}
